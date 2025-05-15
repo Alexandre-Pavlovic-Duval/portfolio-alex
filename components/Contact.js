3a64d47f0a1fc828f95363f64caaebff7475c4cc
Alexandre Pavlovic-Duval <alexandre.pavlovic.duval@gmail.com>
2025-05-15 23:02:40 +0200

    Updated website styling with a new color palette

--- components/Contact.js
+++ components/Contact.js
@@ -2,13 +2,14 @@ import Headers from './Headers';
 import styles from '../styles/Contact.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin, faGithub, faPadlet } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function contactPage() {
     const github= <FontAwesomeIcon icon={faGithub} className={styles.gitHubLogo}/>
     const linkedin= <FontAwesomeIcon icon={faLinkedin} className= {styles.LinkedInLogo} />
     const email = <FontAwesomeIcon icon={faEnvelope} color='#102A42' className={styles.mailIcon}/> 
     const phone = <FontAwesomeIcon icon={faPhone} color='#102A42' className={styles.phoneIcon}/> 
+
     return (
         <div className={styles.mainDiv}>
             <Headers />
@@ -21,26 +22,30 @@ export default function contactPage() {
                         <div className={styles.contactInformation}>
                             <div className={styles.headerInformation}>
                                 <span className={styles.headerInformationTxt}>Alexandre Pavlovic-Duval</span>
-                                <br></br>
+                                <br />
                                 <span className={styles.headerInformationTxtSecond}>Don't hesitate to contact me</span>
                             </div>
-                            <div className={styles.emailInformation}>
-                                <span>{phone}</span><span className={styles.phone}>+33 6 67 67 22 67</span>
-                                <br></br>
-                                <span>{email}</span><span className={styles.mail}>alexandre.pavlovic.duval@gmail.com</span>
-                            </div>
+                            <div className={styles.information}>
+                                <div className={styles.phoneContainer}>
+                                    <span>{phone}</span>
+                                    <span className={styles.phone}>+33 6 67 67 22 67</span>
+                                </div>
+                                <div className={styles.mailContainer}>
+                                    <span>{email}</span>
+                                    <span className={styles.mail}>alexandre.pavlovic.duval@gmail.com</span>
+                                </div>
+                            </div> 
                             <div className={styles.bottomInformation}>
                                 <span className={styles.endInformation}>You can also reach me on</span>
                                 <div className={styles.logoInformation}>
-                                    <div className= {styles.gitHubLink}>{github}</div>
-                                    <div className= {styles.linkedInLink}>{linkedin}</div>
+                                    <div className={styles.gitHubLink}>{github}</div>
+                                    <div className={styles.linkedInLink}>{linkedin}</div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className={styles.rightContainer}>
-
                 </div>
             </div>
         </div>
--- components/Headers.js
+++ components/Headers.js
@@ -3,24 +3,27 @@ import Link from 'next/link';
 
 export default function Header() {
     return (
-        <header className= {styles.header}>
-            <div className= {styles.leftPartHeader}>
-            <div className= {styles.logoHeader}>APD</div>
-            </div>
+        <header className={styles.headerContainer}>
+            <div className= {styles.header}>
+                <div className= {styles.leftPartHeader}>
+                    <div className= {styles.logoHeader}>APD</div>
+                </div>
 
-            <div className= {styles.rightPartHeader}>
-            <Link href="/" >
-                <div className= {styles.aboutHeader}>About</div>
-            </Link>
-            <div className= {styles.projectsHeader}>Projects</div>
-            <Link href="/contact">
-                <div className= {styles.contactHeader}>Contact</div>
-            </Link>
-            <div className= {styles.translationHeader}>
-                Language 🌎
-            </div>
-            <div className= {styles.darkLightModeHeader}>D/L mode 🌙</div>
+                <div className= {styles.rightPartHeader}>
+                    <Link href="/" >
+                        <div className= {styles.aboutHeader}>About</div>
+                    </Link>
+                    <div className= {styles.projectsHeader}>Projects</div>
+                    <Link href="/contact">
+                        <div className= {styles.contactHeader}>Contact</div>
+                    </Link>
+                    <div className= {styles.translationHeader}>
+                        Language 🌎
+                    </div>
+                    <div className= {styles.darkLightModeHeader}>D/L mode 🌙</div>
+                </div>
             </div>
         </header>
+
     )
 };
